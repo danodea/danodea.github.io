4037140824893cb65c75aef60b28dabfefb5adad
danodea <danodea@gmail.com>
2015-06-08 19:24:42 -0400

    Implemented search feature

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+
+
 var Brewery = function(data) {
   var self = this;
   this.name = ko.observable(data.name);
   this.address = ko.observable(data.address);
   this.latLng = ko.observable(new google.maps.LatLng(data.lat, data.lng));
-  this.visible = ko.observable(true);
 
   this.marker = new google.maps.Marker({
       position: this.latLng(),
-      map: map,
+      map: null,
       title: this.name()
   });
 };
@@ -25,18 +26,32 @@ var infowindow = new google.maps.InfoWindow();
 var ViewModel = function() {
   var self = this;
 
-  self.locations = ko.observableArray([]);
+  this.searchString = ko.observable('');
+  this.locations = ko.observableArray([]);
 
   breweries.forEach(function(breweryData) {
     self.locations.push( new Brewery(breweryData) )
   });
 
-  self.locations().forEach(function(brewery) {
+  this.locations().forEach(function(brewery) {
     google.maps.event.addListener(brewery.marker, 'click', function() {
       self.handleClick(brewery);
     });
   });
-  
+
+  this.filteredLocations = ko.computed(function() {
+    var searchedBreweries = [];
+    for (i = 0; i < self.locations().length; i++) {
+        if (self.locations()[i].name().toLowerCase().indexOf(self.searchString().toLowerCase()) != -1) {
+          searchedBreweries.push(self.locations()[i]);
+          self.locations()[i].marker.setMap(map);
+        } else {
+          self.locations()[i].marker.setMap(null);
+        }
+    }
+    return searchedBreweries.sort(function (l, r) { return l.name() > r.name() ? 1 : -1 });
+  })
+
   this.handleClick = function(brewery) {
     map.setZoom(14);
     map.setCenter(brewery.latLng());
@@ -49,3 +64,8 @@ var ViewModel = function() {
 }
 
 ko.applyBindings( new ViewModel() );
+
+
+
+
+
