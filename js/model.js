3cfaca2987ce971aad4bc7f66a88be6430c2132e
danodea <danodea@gmail.com>
2015-06-14 11:02:51 -0400

    Add Yelp Business IDs to model data

--- js/model.js
+++ js/model.js
@@ -3,102 +3,119 @@ var breweries = [
     name: "Sun King Brewing",
     address: "135 N College Ave, Indianapolis, IN, 46202",
     lat: 39.7690233,
-    lng: -86.1446207
+    lng: -86.1446207,
+    yelpID: "sun-king-brewing-indianapolis"
   },
   {
     name: "Flat 12 Bierwerks",
     address: "414 Dorman St, Indianapolis, IN, 46202",
     lat: 39.772835,
-    lng: -86.138683
+    lng: -86.138683,
+    yelpID: "flat12-bierwerks-indianapolis-indianapolis-2"
   },
   {
     name: "Indiana City Brewing",
     address: "24 Shelby St, Indianapolis, IN, 46202",
     lat: 39.7658254,
-    lng: -86.1408132
+    lng: -86.1408132,
+    yelpID: "indiana-city-brewing-company-indianapolis"
   },
   {
     name: "Bier Brewery",
     address: "5133 E 65th St, Indianapolis, IN, 46220",
     lat: 39.8756046,
-    lng: -86.0828565
+    lng: -86.0828565,
+    yelpID: "bier-brewery-and-tap-room-indianapolis"
   },
   {
     name: "Triton Brewing Company",
     address: "5764 Wheeler Rd, Indianapolis, IN, 46216",
     lat: 39.860189,
-    lng: -86.007047
+    lng: -86.007047,
+    yelpID: "triton-brewing-company-indianapolis"
   },
   {
     name: "Upland Brewing Company",
     address: "4842 N College Ave, Indianapolis, IN, 46205",
     lat: 39.8426549,
-    lng: -86.1458764
+    lng: -86.1458764,
+    yelpID: "upland-brewing-co-tasting-room-indianapolis"
   },
   {
     name: "Fountain Square Brewery",
     address: "1301 Barth Ave, Indianapolis, IN, 46203",
     lat: 39.749593,
-    lng: -86.140525
+    lng: -86.140525,
+    yelpID: "fountain-square-brewing-indianapolis"
   },
   {
     name: "Outliers Brewing Company",
     address: "534 North St, Indianapolis, IN, 46204",
     lat: 39.775265,
-    lng: -86.1474455
+    lng: -86.1474455,
+    yelpID: "outliers-brewing-indianapolis"
   },
   {
     name: "Black Acre Brewing",
     address: "5632 E Washington St, Indianapolis, IN, 46219",
     lat: 39.7709647,
-    lng: -86.0709176
+    lng: -86.0709176,
+    yelpID: "black-acre-brewing-indianapolis-2"
   },
   {
     name: "TwoDEEP Brewing Company",
     address: "714 N Capitol Ave, Indianapolis, IN, 46204",
     lat: 39.777248,
-    lng: -86.161837
+    lng: -86.161837,
+    yelpID: "twodeep-brewing-co-indianapolis-2"
   },
   {
     name: "Tow Yard Brewing Company",
     address: "501 Madison Ave, Indianapolis, IN, 46225",
     lat: 39.759948,
-    lng: -86.156913
+    lng: -86.156913,
+    yelpID: "tow-yard-brewing-indianapolis"
   },
   {
     name: "New Day Meadery",
     address: "1102 Prospect St, Indianapolis, IN, 46203",
     lat: 39.752579,
-    lng: -86.139644 
+    lng: -86.139644,
+    yelpID: "new-day-meadery-indianapolis-2"
   },
   {
     name: "Wabash Brewing",
     address: "5328 W 79th St, Indianapolis, IN, 46268",
     lat: 39.8963849,
-    lng: -86.25316078 
+    lng: -86.25316078,
+    yelpID: "wabash-brewing-indianapolis"
   },
   {
     name: "Brugge Brasserie",
     address: "1011a E Westfield Blvd, Indianapolis, IN, 46220",
     lat: 39.8705353,
-    lng: -86.1411883 
+    lng: -86.1411883,
+    yelpID: "brugge-brasserie-indianapolis"
   },
   {
     name: "Books and Brews",
     address: "9402 Uptown Dr, Ste 1400, Indianapolis, IN, 46256",
     lat: 39.9242458,
-    lng: -86.0362054
+    lng: -86.0362054,
+    yelpID: "books-and-brews-indianapolis-2"
   },
   {
     name: "Thr3e Wise Men Brewing Company",
     address: "1021 Broad Ripple Ave, Indianapolis, IN, 46220",
     lat: 39.8694841,
-    lng: -86.1407477
+    lng: -86.1407477,
+    yelpID: "thr3e-wise-men-brewing-company-indianapolis"
   },
   {
     name: "Chilly Water Brewing Company",
     address: "719 Virginia Ave, Ste 105, Indianapolis, IN, 46203",
     lat: 39.7571583,
-    lng:  -86.1457019
+    lng:  -86.1457019,
+    yelpID: "chilly-water-brewing-company-indianapolis-3"
   },
 ];
